b4ae84559f0553b4137b667e12f4bcf7d4e1ddb5
Jered <dmciner1@jhu.edu>
2017-06-20 16:57:20 -0400

    changed categories2

--- scripts/get_categories2.py
+++ scripts/get_categories2.py
@@ -9,10 +9,17 @@ if __name__ == '__main__':
 	args = parser.parse_args()
 
 	output_file = "categories.txt"
-
+	number_of_clusters = 100
+	cluster_mappings_file = "cluster_mappings.txt"
+	cluster_groupings_file = "cluster_groupings.txt"
+	cluster_names_file = "cluster_names.txt"
+	topological_sorting_file = "topological_sorting.txt"
+	
 	G = graph_manip.load_graph(args.adjacencies_file)
-	new_graph = graph_manip.get_n_level_graph_from(G, "Category:Main_topic_classifications", 3)
-	print(len(new_graph))
+	new_graph = graph_manip.get_n_level_graph_from(G, "Category:Main_topic_classifications", 5)
+
 	with open(output_file, "w") as outfile:
 		for node in new_graph.nodes():
 			outfile.write(node+"\n")
+
+	graph_manip.compute_clustering(original_graph, components, number_of_clusters, cluster_mappings_file, cluster_groupings_file, cluster_names_file, topological_sorting_file)
