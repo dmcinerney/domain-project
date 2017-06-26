6d257568f7056bd463bc0cd8870059df9bb966d3
Jered <dmciner1@jhu.edu>
2017-06-26 16:49:08 -0400

    changes

--- python/Preprocessing/train_classifiers.py
+++ python/Preprocessing/train_classifiers.py
@@ -5,9 +5,10 @@ if __name__ == '__main__':
 	parser = argparse.ArgumentParser()
 	parser.add_argument("dataset_file")
 	parser.add_argument("-c", "--classifier_type", type=str, default="svm")
-	parser.add_argument("-p", "--preprocessing_type", type=str, default="lda")
+	parser.add_argument("-p", "--preprocessing_method", type=str, default="lda")
 
 	args = parser.parse_args()
 
 	with open(dataset_file, "r") as dataset:
-		pass
+		print("reading csv file ("+dataset_file+")")
+		df = pd.read_csv(dataset)
